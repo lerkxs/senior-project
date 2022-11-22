b93c97d842aa456d21a88fdbfa1ce59bf0dddfc0
lerkxs <lerkxs@gmail.com>
2022-11-22 10:44:10 +0900

    1122

--- information/views.py
+++ information/views.py
@@ -1,11 +1,9 @@
-# from django.shortcuts import render
-
-# # Create your views here.
-
 from django.views import generic
+# 未ログインユーザーをログインページにリダイレクトさせる(第一引数に指定)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class SkinView(generic.TemplateView):
     template_name = "SkinDiseaseInformation.html"
 
-# class IndexView(generic.TemplateView):
-#     template_name = "AtopicSkin.html"
+class AtopicView(generic.TemplateView):
+    template_name = "AtopicSkin.html"
